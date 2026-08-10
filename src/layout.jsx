3958ab03dc8db7fdff280dae9082396db142ce2a
hitesh <bhatnagerhitesh@gmail.com>
2026-08-10 19:32:55 +0530

    setup project

---
+++ src/layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+function Layout() {
+    return (
+        <>
+            <Navbar />
+            <Outlet />
+            <Footer />
+        </>
+    )
+}
+
+export default Layout
